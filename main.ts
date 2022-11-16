bab03f37a6e0c14d97ef2a10907fa419763ea316
elixrdgtl <107235332+elixrdgtl@users.noreply.github.com>
2022-11-16 10:14:26 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,60 @@
+let number = 0
+input.onGesture(Gesture.ScreenDown, function () {
+    number = 0
+})
+input.onGesture(Gesture.Shake, function () {
+    number = randint(1, 6)
+})
+basic.forever(function () {
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
